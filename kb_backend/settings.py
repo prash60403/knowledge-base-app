f457236c665dd46d4427c15930d1ba284d7f8662
Prashanth HV <prashanthhv2003@gmail.com>
2025-06-30 15:22:40 +0530

    Configure Gmail SMTP and add password reset routes

--- kb_backend/settings.py
+++ kb_backend/settings.py
@@ -97,6 +97,13 @@ DATABASES = {
     }
 }
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'your-email@gmail.com'
+EMAIL_HOST_PASSWORD = 'your-app-password'
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
