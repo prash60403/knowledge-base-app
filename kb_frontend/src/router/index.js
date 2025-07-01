9ad24427487b4b9dd7b8776ced78278f49c244b2
Prashanth HV <prashanthhv2003@gmail.com>
2025-07-01 17:45:26 +0530

    index.html change

--- kb_frontend/src/router/index.js
+++ kb_frontend/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import App from '/App.vue'
+import App from '@/App.vue';  
 import PrashLogin from '@/views/PrashLogin.vue';
 import CustomerRegister from '@/views/CustomerRegister.vue';
 
