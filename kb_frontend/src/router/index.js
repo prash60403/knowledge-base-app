b468851deb63f88fbdb7c69d18479a37c48b1e92
Prashanth HV <prashanthhv2003@gmail.com>
2025-07-01 15:10:00 +0530

    Add the features

--- kb_frontend/src/router/index.js
+++ kb_frontend/src/router/index.js
@@ -1,30 +1,23 @@
-import Vue from 'vue';
-import Router from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import PrashLogin from '@/views/PrashLogin.vue';
 import CustomerRegister from '@/views/CustomerRegister.vue';
 
-Vue.use(Router);
+const routes = [
+  {
+    path: '/',
+    name: 'Login',
+    component: PrashLogin
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: CustomerRegister
+  }
+];
 
-export default new Router({
-  mode: 'history', // Important for clean URLs on Render
-  routes: [
-    {
-      path: '/',
-      redirect: '/login'
-    },
-    {
-      path: '/login',
-      name: 'Login',
-      component: PrashLogin
-    },
-    {
-      path: '/register',
-      name: 'Register',
-      component: CustomerRegister
-    },
-    {
-      path: '*',
-      redirect: '/login'
-    }
-  ]
+const router = createRouter({
+  history: createWebHistory(),
+  routes
 });
+
+export default router;
