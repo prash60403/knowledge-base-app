1f57827589261e03b38973a06468ac917d23592e
Prashanth HV <prashanthhv2003@gmail.com>
2025-07-03 12:13:45 +0530

    setting changes

--- kb_backend/settings.py
+++ kb_backend/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 from decouple import config
 import os
 ALLOWED_HOSTS = [
-    'your-backend-name.onrender.com',
+    'kb-backend-name.onrender.com',
     'localhost',
     '127.0.0.1'
 ]
